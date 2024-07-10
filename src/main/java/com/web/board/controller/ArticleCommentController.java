5b7f8695d6e401d43dd1a450bcfcba037742b27c
ekgus419 <ekgus419@gmail.comeqw>
2024-07-10 23:21:35 +0900

    #26 - 댓글 컨트롤러 리팩토링 : 빈줄 삭

--- src/main/java/com/web/board/controller/ArticleCommentController.java
+++ src/main/java/com/web/board/controller/ArticleCommentController.java
@@ -27,7 +27,6 @@ public class ArticleCommentController {
     ) {
         articleCommentService.saveArticleComment(articleCommentRequest.toDto(boardPrincipal.toDto()));
 
-
         return "redirect:/articles/" + articleCommentRequest.articleId();
     }
 
